be04aef45fee54c14e4b8e50a73f0feda7a33ca7
Vjeko-Oresic <vjeko.oresic@gmail.com>
2014-06-11 17:18:52 +0200

    Add project

---
+++ ProjektII/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace ProjektII
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b_Ucitaj = new System.Windows.Forms.Button();
+            this.listVijest = new System.Windows.Forms.ListView();
+            this.lblNaziv = new System.Windows.Forms.Label();
+            this.lblOpis = new System.Windows.Forms.Label();
+            this.txtLink = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtOpis = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // b_Ucitaj
+            // 
+            this.b_Ucitaj.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.b_Ucitaj.Location = new System.Drawing.Point(554, 10);
+            this.b_Ucitaj.Name = "b_Ucitaj";
+            this.b_Ucitaj.Size = new System.Drawing.Size(75, 23);
+            this.b_Ucitaj.TabIndex = 0;
+            this.b_Ucitaj.Text = "Učitaj";
+            this.b_Ucitaj.UseVisualStyleBackColor = true;
+            this.b_Ucitaj.Click += new System.EventHandler(this.b_Ucitaj_Click);
+            // 
+            // listVijest
+            // 
+            this.listVijest.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.listVijest.Location = new System.Drawing.Point(12, 96);
+            this.listVijest.Name = "listVijest";
+            this.listVijest.Size = new System.Drawing.Size(617, 170);
+            this.listVijest.TabIndex = 4;
+            this.listVijest.UseCompatibleStateImageBehavior = false;
+            this.listVijest.View = System.Windows.Forms.View.List;
+            this.listVijest.SelectedIndexChanged += new System.EventHandler(this.listVijest_SelectedIndexChanged);
+            this.listVijest.DoubleClick += new System.EventHandler(this.listVijest_DoubleClick);
+            // 
+            // lblNaziv
+            // 
+            this.lblNaziv.AutoSize = true;
+            this.lblNaziv.Location = new System.Drawing.Point(9, 42);
+            this.lblNaziv.Name = "lblNaziv";
+            this.lblNaziv.Size = new System.Drawing.Size(37, 13);
+            this.lblNaziv.TabIndex = 5;
+            this.lblNaziv.Text = "Naziv:";
+            // 
+            // lblOpis
+            // 
+            this.lblOpis.AutoSize = true;
+            this.lblOpis.Location = new System.Drawing.Point(9, 68);
+            this.lblOpis.Name = "lblOpis";
+            this.lblOpis.Size = new System.Drawing.Size(31, 13);
+            this.lblOpis.TabIndex = 7;
+            this.lblOpis.Text = "Opis:";
+            // 
+            // txtLink
+            // 
+            this.txtLink.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLink.Location = new System.Drawing.Point(68, 12);
+            this.txtLink.Name = "txtLink";
+            this.txtLink.Size = new System.Drawing.Size(480, 20);
+            this.txtLink.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "RSS feed:";
+            // 
+            // txtOpis
+            // 
+            this.txtOpis.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOpis.Location = new System.Drawing.Point(12, 272);
+            this.txtOpis.Name = "txtOpis";
+            this.txtOpis.ReadOnly = true;
+            this.txtOpis.Size = new System.Drawing.Size(617, 67);
+            this.txtOpis.TabIndex = 11;
+            this.txtOpis.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 351);
+            this.Controls.Add(this.txtOpis);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLink);
+            this.Controls.Add(this.lblOpis);
+            this.Controls.Add(this.lblNaziv);
+            this.Controls.Add(this.listVijest);
+            this.Controls.Add(this.b_Ucitaj);
+            this.Name = "Form1";
+            this.Text = "RSS Reader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b_Ucitaj;
+        private System.Windows.Forms.ListView listVijest;
+        private System.Windows.Forms.Label lblNaziv;
+        private System.Windows.Forms.Label lblOpis;
+        private System.Windows.Forms.TextBox txtLink;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox txtOpis;
+    }
+}
+
---
+++ ProjektII/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace ProjektII
+{
+    public partial class Form1 : Form
+    {
+        XmlTextReader rssReader;
+        XmlDocument rssDoc;
+        XmlNode nodeRss;
+        XmlNode nodeChannel;
+        XmlNode nodeItem;
+        ListViewItem rowList;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void b_Ucitaj_Click(object sender, EventArgs e)
+        {
+            //Čišćenje liste ako u njoj postoje već neki Items
+            listVijest.Items.Clear();
+            this.Cursor = Cursors.WaitCursor;
+            //Kreiranje XmlTextReader za uneseni link
+            rssReader = new XmlTextReader(txtLink.Text);
+            rssDoc = new XmlDocument();
+            //Load sadržaja XML-a u XmlDocument
+            rssDoc.Load(rssReader);
+            //Petlja za <rss>
+            for (int i = 0; i < rssDoc.ChildNodes.Count; i++)
+            {
+                if (rssDoc.ChildNodes[i].Name == "rss")
+                {
+                    nodeRss = rssDoc.ChildNodes[i];
+                }
+            }
+            //Petlja za <channel>
+            for (int i = 0; i < nodeRss.ChildNodes.Count; i++)
+            {
+                if (nodeRss.ChildNodes[i].Name == "channel")
+                {
+                    nodeChannel = nodeRss.ChildNodes[i];
+                }
+            }
+            //Labele s informacijama
+            lblNaziv.Text = "Naziv: " + nodeChannel["title"].InnerText;
+            lblOpis.Text = "Opis: " + nodeChannel["description"].InnerText;
+            //Petlja za <item>
+            for (int i = 0; i < nodeChannel.ChildNodes.Count; i++)
+            {
+                if (nodeChannel.ChildNodes[i].Name == "item")
+                {
+                    nodeItem = nodeChannel.ChildNodes[i];
+                    //Kreira nove redove u ListView s informacijama iz nodeItem
+                    rowList = new ListViewItem();
+                    rowList.Text = nodeItem["title"].InnerText;
+                    rowList.SubItems.Add(nodeItem["link"].InnerText);
+                    listVijest.Items.Add(rowList);
+                }
+            }
+            this.Cursor = Cursors.Default;
+        }
+
+        private void listVijest_DoubleClick(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(listVijest.SelectedItems[0].SubItems[1].Text);
+        }
+
+        private void listVijest_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listVijest.SelectedItems.Count == 1)
+            {
+                //Petlja kroz <channel> dok ne naiđe na <item>
+                for (int i = 0; i < nodeChannel.ChildNodes.Count; i++)
+                {
+                    if (nodeChannel.ChildNodes[i].Name == "item")
+                    {
+                        nodeItem = nodeChannel.ChildNodes[i];
+                        //Ako je <title> jednak selektiranom u ListView prikazat će description u txtOpis
+                        if (nodeItem["title"].InnerText == listVijest.SelectedItems[0].Text)
+                        {
+                            txtOpis.Text = nodeItem["description"].InnerText;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
